05e64bed8c1560b122cec513d4472155f3092c2b
Mitchell Reiff <me@mreiff.dev>
2023-01-19 16:58:36 -0500

    HW0

---
+++ Assignments/helloworld-MAPReiff/build/src/CMakeFiles/fullName_func.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fullName_func.dir/fullName.cpp.o"
+  "CMakeFiles/fullName_func.dir/fullName.cpp.o.d"
+  "libfullName_func.a"
+  "libfullName_func.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fullName_func.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Assignments/helloworld-MAPReiff/build/src/CMakeFiles/fullName_func.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for fullName_func.
+# This may be replaced when dependencies are built.
